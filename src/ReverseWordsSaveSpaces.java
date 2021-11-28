bcfdba138640264453a6e226c9d62b0fc8a4b7d8
MarkKovsk <kovskmark@gmail.com>
2021-11-29 00:39:06 +0700

    1

--- src/ReverseWordsSaveSpaces.java
+++ src/ReverseWordsSaveSpaces.java
@@ -1,21 +1,21 @@
-/* Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
-        Examples
-        "This is an example!" ==> "sihT si na !elpmaxe"
-        "double  spaces"      ==> "elbuod  secaps"
-
-*/
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReverseWordsSaveSpaces {
     public static void main(String[] args) {
+        System.out.println(Kata2.factorial(-1));
 
-        System.out.println(Kata.reverseWords("This is an  example!"));
     }
 }
 
+
+/* Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+        Examples
+        "This is an example!" ==> "sihT si na !elpmaxe"
+        "double  spaces"      ==> "elbuod  secaps"
+*/
 class Kata {
     public static String reverseWords(final String original) {
         StringBuilder sb = new StringBuilder(original);
@@ -49,3 +49,15 @@ class Kata {
     }
 }
 
+// factorial
+class Kata2 {
+    public static int factorial(int n) {
+        if (n < 0 || n > 12) {
+            throw new IllegalArgumentException();
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}
